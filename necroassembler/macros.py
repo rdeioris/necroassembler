f1139a8faa9336e5875e1688602bf7d4811040bb
Roberto De Ioris <roberto.deioris@gmail.com>
2019-10-11 09:54:22 +0200

    refactored macro

--- necroassembler/macros.py
+++ necroassembler/macros.py
@@ -1,12 +1,26 @@
+'''Assembler Macro system'''
+
+
 class Macro:
+    '''Represents a user-defined macro'''
+
     def __init__(self, tokens):
         self.name, *self.args = tokens
         self.instructions = []
 
     def add_instruction(self, instr):
+        """Appends an instruction to a macro
+
+        :param statements.Instruction instr: the Instrction to add, generally built by the Tokenizer
+        """
         self.instructions.append(instr)
 
     def assemble(self, assembler, tokens):
+        """Assembles a macro using the specified assembler
+
+        :param assembler.Assembler assembler: the assembler to use
+        :param list tokens: the tokens used to invoke the macro (macro name included)
+        """
         _, *args = tokens
         macro_args = self.args
         for instr in self.instructions:
