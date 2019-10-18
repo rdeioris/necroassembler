34db75dc405ee521649aadeb633dd13a3d20fe45
Roberto De Ioris <roberto.deioris@gmail.com>
2019-10-18 19:35:00 +0200

    added nes assembler binary

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(name='necroassembler',
       entry_points={
           'console_scripts': [
               'necro_6502=necroassembler.cpu.mos6502:main',
+              'necro_nes=necroassembler.platforms.nes:main',
               'necro_gb=necroassembler.platforms.gameboy:main',
               'necro_gba=necroassembler.platforms.gba:main'
           ],
