e7de4ea0a5860c1d58602dd7ac297c5982cc73e5
Roberto De Ioris <roberto.deioris@gmail.com>
2019-10-23 11:35:33 +0200

    updated to 0.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='necroassembler',
-      version='0.3.2',
+      version='0.4',
       description='framework for building assemblers',
       url='https://github.com/rdeioris/necroassembler/',
       author='Roberto De Ioris',
